5fc10bbdf54f982d9f1c7ddbeec02a94833bfd28
Jasper van Merle <jaspervmerle@gmail.com>
2016-10-24 15:20:39 +0200

    Create HexEnhancedPlus.meta.js

---
+++ HexEnhancedPlus.meta.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         HexEnhanced+
+// @namespace    HexEnhancedPlus
+// @version      1.0.0
+// @description  HexEnhanced+ adds a load of features to Hacker Experience 1 and fixes some bugs aswell.
+// @author       MacHacker, Jasperr & Johannes
+// @match        https://*.hackerexperience.com/*
+// @updateURL    https://gitcdn.xyz/repo/Johannes2306/Hex-Enhanced-Plus/master/HexEnhancedPlus.meta.js
+// @downloadURL  https://gitcdn.xyz/repo/Johannes2306/Hex-Enhanced-Plus/master/HexEnhancedPlus.user.js
+// @grant        none
+// ==/UserScript==
